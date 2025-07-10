737128894bddb5372704f46c070eed29501f15f9
john Howorth <johnhoworth2024@gmail.com>
2025-07-10 09:20:49 +0000

    Now i can't see the player ? Give the player some 3d depth in the z axis

--- src/components/icons/PlayerIcon.tsx
+++ src/components/icons/PlayerIcon.tsx
@@ -7,21 +7,35 @@ export default function PlayerIcon({ className }: { className?: string }) {
         >
             <defs>
                 <filter id="glow" x="-50%" y="-50%" width="200%" height="200%">
-                    <feGaussianBlur stdDeviation="3" result="coloredBlur" />
+                    <feGaussianBlur stdDeviation="2" result="coloredBlur" />
                     <feMerge>
                         <feMergeNode in="coloredBlur" />
                         <feMergeNode in="SourceGraphic" />
                     </feMerge>
                 </filter>
             </defs>
+            
+            {/* 3D effect layers */}
+            <g transform="translate(0, 4)">
+                 <path d="M24 34 L14 28 L14 16 L24 10 L34 16 L34 28 Z" fill="hsl(var(--accent) / 0.3)" />
+            </g>
+            <g transform="translate(0, 2)">
+                 <path d="M24 34 L14 28 L14 16 L24 10 L34 16 L34 28 Z" fill="hsl(var(--accent) / 0.5)" />
+            </g>
+             <g transform="translate(0, 0)">
+                 <path d="M24 34 L14 28 L14 16 L24 10 L34 16 L34 28 Z" fill="hsl(var(--accent) / 0.7)" />
+            </g>
+
             {/* Base shape */}
-            <path d="M24 34 L14 28 L14 16 L24 10 L34 16 L34 28 Z" fill="hsl(var(--accent))" filter="url(#glow)" />
-            {/* Top face */}
-            <path d="M24 10 L34 16 L24 22 L14 16 Z" fill="hsl(var(--accent) / 0.7)" />
-            {/* Side face */}
-            <path d="M34 16 L34 28 L24 34 L24 22 Z" fill="hsl(var(--accent) / 0.5)" />
-            {/* Inner highlight */}
-            <path d="M24 30 L18 27 L18 19 L24 16 L30 19 L30 27 Z" fill="hsl(var(--primary))" />
+            <g transform="translate(0, -2)" filter="url(#glow)">
+                <path d="M24 34 L14 28 L14 16 L24 10 L34 16 L34 28 Z" fill="hsl(var(--accent))" />
+                {/* Top face */}
+                <path d="M24 10 L34 16 L24 22 L14 16 Z" fill="hsl(var(--accent) / 0.7)" />
+                {/* Side face */}
+                <path d="M34 16 L34 28 L24 34 L24 22 Z" fill="hsl(var(--accent) / 0.5)" />
+                {/* Inner highlight */}
+                <path d="M24 30 L18 27 L18 19 L24 16 L30 19 L30 27 Z" fill="hsl(var(--primary))" />
+            </g>
         </svg>
     );
 }

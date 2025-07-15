a99c0cc1781a98892e98a3b759c88b33410bc2f7
john Howorth <johnhoworth2024@gmail.com>
2025-07-15 17:34:41 +0000

    The FlashingPill Icon is too dull on screen, make it glow more brightly.

--- src/components/icons/FlashingPillIcon.tsx
+++ src/components/icons/FlashingPillIcon.tsx
@@ -11,13 +11,21 @@ export default function FlashingPillIcon({ className }: { className?: string }) 
             strokeLinecap="round"
             strokeLinejoin="round"
         >
+            <defs>
+                <filter id="glow-red" x="-50%" y="-50%" width="200%" height="200%">
+                    <feDropShadow dx="0" dy="0" stdDeviation="2" floodColor="#ff0000" />
+                </filter>
+                <filter id="glow-blue" x="-50%" y="-50%" width="200%" height="200%">
+                    <feDropShadow dx="0" dy="0" stdDeviation="2" floodColor="#0000ff" />
+                </filter>
+            </defs>
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <g className="siren-red">
-                <path d="M4.5 12.5l8 8" stroke="#ff0000" fill="#ff0000" />
+                <path d="M4.5 12.5l8 8" stroke="#ff0000" fill="#ff0000" style={{ filter: 'url(#glow-red)' }} />
                 <path d="M8.5 8.5l7 7" stroke="#ff0000" fill="#ff0000" opacity="0" />
             </g>
              <g className="siren-blue">
-                <path d="M19.5 4.5l-8 8" stroke="#0000ff" fill="#0000ff" />
+                <path d="M19.5 4.5l-8 8" stroke="#0000ff" fill="#0000ff" style={{ filter: 'url(#glow-blue)' }} />
                 <path d="M15.5 15.5l-7-7" stroke="#0000ff" fill="#0000ff" opacity="0" />
             </g>
         </svg>
